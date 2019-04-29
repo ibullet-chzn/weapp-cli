1f0490353b5a30b219484c9fb042de5ecade077d
于超 <ChaoYu@sf-express.com>
2019-04-29 16:09:49 +0800

    readme

--- miniprogram/src/utils/encrypt.js
+++ miniprogram/src/utils/encrypt.js
@@ -1,9 +1,9 @@
 import CryptoJS from '../lib/CryptoJS';
 
 const key = CryptoJS.enc.Hex.parse(
-  '8e3e16074b3c9833c981f047bf79fb95607d7e624cb03b0a105311a766d46f28',
+  '123-test',
 );
-const iv = CryptoJS.enc.Hex.parse('0000000000000000000000000000000000000000000000000000000000000000');
+const iv = CryptoJS.enc.Hex.parse('0');
 
 const encrypt = (word) => {
   const src = CryptoJS.enc.Utf8.parse(word);
