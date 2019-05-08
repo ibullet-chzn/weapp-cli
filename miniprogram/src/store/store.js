5be62023ffd87423be9cfc2f58dc6c72d4987eca
于超 <ChaoYu@sf-express.com>
2019-05-08 11:15:53 +0800

    redux优化

--- miniprogram/src/store/store.js
+++ miniprogram/src/store/store.js
@@ -3,10 +3,19 @@ import { withStore } from '../lib/retalk';
 import store from './index';
 
 /**
- * 未解决store在非page页面无法使用而额外提供的函数
+ * 提供一个静态方法 解决 request store 相互调用的问题
+ */
+class Store {
+  static all() {
+    return connect(
+      ...withStore('Common', 'Address'),
+      store,
+    )({});
+  }
+}
+
+/**
+ * 解决store在非page页面无法使用而额外提供的函数
  * !!! wechat-weapp-redux 依赖库有修改 (connect 方法有兼容性修改) !!!
  */
-export default connect(
-  ...withStore('common'),
-  store,
-)({});
+export default Store;
